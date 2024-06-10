d65eed872bda27ce6adc9794b7804ff5968e6fe9
Sekonso <ancaadri9@gmail.com>
2024-06-11 00:01:40 +0700

    CORS fix

--- src/index.js
+++ src/index.js
@@ -19,8 +19,20 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 const port = process.env.PORT || 2024;
 
+// CORS
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: "GET,POST,PUT,PATCH,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
